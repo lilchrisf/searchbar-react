804104d317eb910b7bf0e4f75eb6ea1d4fc6848d
Christopher Watson <christopherwatson299@gmail.com>
2022-02-08 07:32:54 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,121 @@
-import logo from './logo.svg';
 import './App.css';
+import React from "react";
+import PropTypes from "prop-types";
+import pokeman from "./pokeman.json"
+// import styled from "@emotion/styled";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const [filter, filterSet ] = React.useState("");
+const [selectedItem, selectedItemSet] = React.useState(null );
+
+const PokemanRow = ({ pokeman, onSelect }) => (
+<tr>
+  <td>{pokeman.name.english}</td>
+  <td>{pokeman.type.join(", ")}</td>
+  <td>
+    <button onClick={() => onSelect(pokeman)}
+    >Select!</button>
+  </td>
+</tr>
+);
+
+PokemanRow.propTypes = {
+  pokeman: PropTypes.shape({
+    name: PropTypes.shape({
+      english: PropTypes.string.isRequired,
+    }),
+    type: PropTypes.arrayOf(PropTypes.string.isRequired),
+  }),
+onSelect: PropTypes.func.isRequired,
 }
 
-export default App;
+const PokemanInfo = ({ name, base}) => (
+  <div>
+    <h1>{name.english}</h1>
+    <table>
+      {
+        Object.keys(base).map((key) => (
+          <tr key={key}>
+            <td>{key}</td>
+            <td>{base[key]}</td>
+          </tr>
+        ))
+      }
+    </table>
+  </div>
+)
+
+PokemanInfo.prototype = {
+  name: PropTypes.shape({
+    english: PropTypes.string.isRequired, 
+  }),
+  base: PropTypes.number.isRequired,
+  HP: PropTypes.number.isRequired,
+  Attack: PropTypes.number.isRequired,
+  Defence: PropTypes.number.isRequired,
+  "Sp. Attack": PropTypes.number.isRequired, 
+  "Sp. Defence": PropTypes.number.isRequired, 
+  speed: PropTypes.number.isRequired,
+}
+
+ 
+return ( 
+<div className="mainApp">
+
+<div
+style={{
+  margin:"auto",
+  paddingtop: "1rem",
+}}
+>
+
+<header className='header'>
+  <h1 className="title"> Car Search</h1>
+<input value={filter} onChange={(evt) => 
+  filterSet(evt.target.value)}/>
+</header>
+
+<div style={{
+  display:"grid",
+  gridTemplateColumns: "60% 30%",
+  gridColumnGap: "1rem",
+}} 
+>
+    
+
+<div> 
+<table width="100%">
+<thead>
+  <tr>
+    <th>Name</th>
+    <th>Brand</th>
+    <th>Action</th>
+  </tr>
+</thead> 
+
+<tbody>
+  {pokeman
+  .filter((pokeman) => 
+  pokeman.name.english.toLowerCase().includes(filter.toLowerCase())
+  )
+  .slice(0,6).map(pokeman => (
+    <PokemanRow pokeman={pokeman}
+    key={pokeman.id} onSelect={(pokeman) => selectedItemSet(pokeman)}/>
+  ))}
+      
+</tbody>
+</table>
+
+  </div> 
+  {selectedItem && <PokemanInfo { ...selectedItem} />}
+</div>
+</div>
+</div>
+ 
+ 
+  );
+} 
+
+export default App;   
